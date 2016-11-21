7c32c812b3ab7e37eb5f33ea2bca26b4a0aa23e4
kanomkrub <tweerath.p@kanomkrub.com>
2016-11-22 02:01:42 +0700

    test .net custom configuration section

---
+++ ConsoleApplication2/MappingPathSection.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Configuration;
+
+namespace ConsoleApplication2
+{
+    public class MappingPathSection : ConfigurationSection
+    {
+        [ConfigurationProperty("", IsDefaultCollection = true)]
+        public BatchCollection Batchs
+        {
+            get
+            {
+                BatchCollection batchCollection = (BatchCollection)base[""];
+                return batchCollection;
+            }
+        }
+    }
+
+    public class BatchCollection : ConfigurationElementCollection
+    {
+        public override ConfigurationElementCollectionType CollectionType
+        {
+            get { return ConfigurationElementCollectionType.BasicMap; }
+        }
+
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new BatchConfigElement();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((BatchConfigElement)element).BatchId;
+        }
+
+        protected override string ElementName
+        {
+            get { return "batch"; }
+        }
+    }
+    public class BatchConfigElement : ConfigurationElement
+    {
+        [ConfigurationProperty("batchId", IsRequired = true, IsKey = true)]
+        public string BatchId
+        {
+            get { return (string)this["batchId"]; }
+            set { this["batchId"] = value; }
+        }
+
+        [ConfigurationProperty("source", IsRequired = false)]
+        public string Source
+        {
+            get { return (string)this["source"]; }
+            set { this["source"] = value; }
+        }
+
+        [ConfigurationProperty("mappings", IsDefaultCollection = false)]
+        public MappingCollection Mappings
+        {
+            get { return (MappingCollection)base["mappings"]; }
+        }
+    }
+    public class MappingCollection : ConfigurationElementCollection
+    {
+
+        public override ConfigurationElementCollectionType CollectionType
+        {
+            get { return ConfigurationElementCollectionType.BasicMap; }
+        }
+
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new MappingConfigElement();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((MappingConfigElement)element).Name;
+        }
+        protected override string ElementName
+        {
+            get { return "mapping"; }
+        }
+    }
+    public class MappingConfigElement: ConfigurationElement
+    {
+        [ConfigurationProperty("name", IsRequired = true, IsKey = true, DefaultValue = "")]
+        public string Name
+        {
+            get { return (string)this["name"]; }
+            set { this["name"] = value; }
+        }
+
+        [ConfigurationProperty("sourcePath", IsRequired = true, DefaultValue = "")]
+        public string SourcePath
+        {
+            get { return (string)this["sourcePath"]; }
+            set { this["sourcePath"] = value; }
+        }
+
+        [ConfigurationProperty("koolKeeperPath", IsRequired = true, DefaultValue = "")]
+        public string KoolkeeperPath
+        {
+            get { return (string)this["koolKeeperPath"]; }
+            set { this["koolKeeperPath"] = value; }
+        }
+
+        [ConfigurationProperty("active", IsRequired = true, DefaultValue = false)]
+        public bool Active
+        {
+            get { return (bool)this["active"]; }
+            set { this["active"] = value; }
+        }
+    }
+}
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MappingPathSection section = ConfigurationManager.GetSection("MappingPathSection") as MappingPathSection;
+            foreach(BatchConfigElement batch in section.Batchs)
+            {
+                Console.WriteLine(batch.BatchId);
+                foreach(MappingConfigElement mapping in batch.Mappings)
+                {
+                    Console.WriteLine("-" + mapping.Name);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
